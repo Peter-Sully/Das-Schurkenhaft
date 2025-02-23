6a857b1b364f72e49a53a29f751db3686b7d583c
Gagana Warusawithana <gagana@Gaganas-MacBook-Pro-2.local>
2025-02-22 18:17:23 -0800

    added two new cards and changed card script; added deck and a script to it; added a card sscript to auto generate cards from list of cards as a gameobject

--- Das-Schurkenhaft/Assets/Scripts/Card.cs
+++ Das-Schurkenhaft/Assets/Scripts/Card.cs
@@ -5,15 +5,40 @@ public class Card : ScriptableObject
 {
     public string cardName;
     public string description;
-    public int damage;
     public Sprite artwork;
     public int cost;
-    public CardType type; // Enum: Attack, Defense, etc.
+    public CardType type; // Attack, Defense, Buff, Debuff, Heal
+    public int value;  // General value for damage, healing, or buffs
 
-    public void PlayCard() 
+    public void PlayCard()
     {
-        Debug.Log($"Played {cardName}!");
-        // Implement card effect logic here
+        switch (type)
+        {
+            case CardType.Attack:
+                Debug.Log($"Played {cardName}: Deals {value} damage.");
+                // Implement attack logic
+                break;
+
+            case CardType.Defense:
+                Debug.Log($"Played {cardName}: Gains {value} shield.");
+                // Implement shield logic
+                break;
+
+            case CardType.Buff:
+                Debug.Log($"Played {cardName}: Boosts stats by {value}.");
+                // Implement stat boost logic
+                break;
+
+            case CardType.Debuff:
+                Debug.Log($"Played {cardName}: Lowers enemy stats by {value}.");
+                // Implement debuff logic
+                break;
+
+            case CardType.Heal:
+                Debug.Log($"Played {cardName}: Restores {value} HP.");
+                // Implement healing logic
+                break;
+        }
     }
 }
 
@@ -22,5 +47,6 @@ public enum CardType
     Attack,
     Defense,
     Buff,
-    Debuff
+    Debuff,
+    Heal
 }
