93bf3050f4f7e4188d9a5b9d82ec544324881fda
Gagana Warusawithana <gagana@Gaganas-MacBook-Pro-2.local>
2025-02-25 15:18:27 -0800

    Cards now appear on hand next to each other around the center, and as more cards appear they overlap with eachother to lessen clutter and allow more cards to be drawn

--- Das-Schurkenhaft/Assets/Scripts/DeckManager.cs
+++ Das-Schurkenhaft/Assets/Scripts/DeckManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 
-public class CardSpawner : MonoBehaviour
+public class DeckManager : MonoBehaviour
 {
     public GameObject cardPrefab;  // Assign your card prefab in the Inspector
     public Transform PlayerHand;  // Assign a UI panel or area where the card should appear
@@ -39,6 +39,7 @@ public class CardSpawner : MonoBehaviour
             deck = RemoveCardFromDeck(deck, 0);
             SpawnCard(drawnCard);
             UpdateDeckSizeUI();
+            UpdateCardLayoutUI();
         }
         else
         {
@@ -77,5 +78,35 @@ public class CardSpawner : MonoBehaviour
     void UpdateDeckSizeUI()
     {
         if (deckSizeText != null) deckSizeText.text = "Deck Size: " + deck.Length;
+    }
+
+    void UpdateCardLayoutUI()
+    {
+        int cardCount = PlayerHand.childCount;
+        if (cardCount == 0) return;
+
+        float panelWidth = PlayerHand.GetComponent<RectTransform>().rect.width;
+        float cardWidth = cardPrefab.GetComponent<RectTransform>().rect.width;
+        
+        float maxSpacing = cardWidth * 0.75f;  // Default spacing (before overlap)
+        float overlapFactor = 0.4f;  // Adjust how much cards overlap when hand is full
+
+        // Calculate total width needed to place all cards with maxSpacing
+        float totalWidth = (cardCount - 1) * maxSpacing + cardWidth;
+
+        // If total width exceeds panel width, calculate overlap
+        float spacing = totalWidth > panelWidth 
+            ? -cardWidth * overlapFactor  // Overlapping mode
+            : maxSpacing;                 // Normal spacing mode
+
+        // Find start position (center first card, then shift left)
+        float startX = -((cardCount - 1) * spacing) / 2f;
+
+        // Position each card
+        for (int i = 0; i < cardCount; i++)
+        {
+            RectTransform cardTransform = PlayerHand.GetChild(i).GetComponent<RectTransform>();
+            cardTransform.anchoredPosition = new Vector2(startX + i * spacing, 0);
+        }
     }
 }
