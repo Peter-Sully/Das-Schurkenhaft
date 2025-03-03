3d1d2afc90463702f858e53651c5a5e7c9ad9e23
KyleTaing <kdtaing621@gmail.com>
2025-03-03 15:08:35 -0800

    NPC Moving

--- Das-Schurkenhaft/Assets/Scripts/NPC.cs
+++ Das-Schurkenhaft/Assets/Scripts/NPC.cs
@@ -1,41 +1,96 @@
-using UnityEngine;
-using System.Collections.Generic;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class NPC
+public class NPC : MonoBehaviour
 {
-    private Vector1 directionVec;
+    private Vector3 directionVector;
     private Transform myTransform;
     public float speed;
-    private NPCbody2D NPCbody;
-    void Start(){
+    private Rigidbody2D myRigidbody;
+    private Animator anim;
+    public Collider2D bounds;
+    public bool playerInRange = false;
+    private bool canMove = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
         myTransform = GetComponent<Transform>();
-        myRigidbody = GetComponent<NPCbody2D>();
+        myRigidbody = GetComponent<Rigidbody2D>();
         ChangeDirection();
     }
-    void Update(){
-        Move();
+
+    // Update is called once per frame for timer logic
+    void Update()
+    {
+        if(!playerInRange){
+            Move();
+        }
+    
     }
-    private void Move(){
-        NPCbody.MovePosition(myTransform.position + directionVec * speed * Time.deltaTime);
+
+
+    private void Move()
+    {
+        // Calculate the new position using FixedDeltaTime.
+        Vector3 newPosition = myTransform.position + directionVector * speed * Time.fixedDeltaTime;
+        // Optional debug log to see where we're trying to move.
+        // Debug.Log("Attempting move to: " + newPosition);
+
+        // Check if the new position is within the allowed bounds.
+        if (bounds.bounds.Contains(newPosition))
+        {
+            myRigidbody.MovePosition(newPosition);
+        }
+        else
+        {
+            // If out of bounds, change direction.
+            ChangeDirection();
+        }
     }
-    void ChangeDirection(){
-        int direction = Random.range(0,4);
-        switch(direction){
+
+    void ChangeDirection()
+    {
+        int direction = Random.Range(0, 4);
+        switch (direction)
+        {
             case 0:
-                directionVec = Vector1.right;
+                directionVector = Vector3.right;
                 break;
             case 1:
-                directionVec = Vector1.up;
+                directionVector = Vector3.up;
                 break;
             case 2:
-                directionVec = Vector1.down;
+                directionVector = Vector3.left;
                 break;
             case 3:
-                directionVec = Vector1.left;
+                directionVector = Vector3.down;
                 break;
             default:
                 break;
         }
+        UpdateAnimation();
     }
-}
+
+    void UpdateAnimation()
+    {
+        if (anim != null)
+        {
+            anim.SetFloat("MoveX", directionVector.x);
+            anim.SetFloat("MoveY", directionVector.y);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Vector3 temp = directionVector;
+        ChangeDirection();
+        int loops = 0;
+        while(temp == directionVector && loops < 100){
+            loops++;
+            ChangeDirection();
+        }
+    }
+} 
