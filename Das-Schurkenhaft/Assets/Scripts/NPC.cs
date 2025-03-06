f7e7c2a1b49b8d78bb8bf577c1307d0d7c9a6425
KyleTaing <kdtaing621@gmail.com>
2025-03-05 20:13:56 -0800

    Textbox added to NPC

--- Das-Schurkenhaft/Assets/Scripts/NPC.cs
+++ Das-Schurkenhaft/Assets/Scripts/NPC.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPC : MonoBehaviour
+public class NPC : Textbox
 {
     private Vector3 directionVector;
     private Transform myTransform;
@@ -10,43 +10,75 @@ public class NPC : MonoBehaviour
     private Rigidbody2D myRigidbody;
     private Animator anim;
     public Collider2D bounds;
-    public bool playerInRange = false;
-    private bool canMove = true;
+    private bool isMoving;
+    public float minMoveTime;
+    public float maxMoveTime;
+    private float moveTimeSeconds;
+    public float minWaitTime;
+    public float maxWaitTime;
+    private float waitTimeSeconds;
 
     // Start is called before the first frame update
     void Start()
     {
+        moveTimeSeconds = Random.Range(minMoveTime, maxMoveTime);
+        waitTimeSeconds = Random.Range(minMoveTime, maxMoveTime);
         anim = GetComponent<Animator>();
         myTransform = GetComponent<Transform>();
         myRigidbody = GetComponent<Rigidbody2D>();
         ChangeDirection();
     }
 
-    // Update is called once per frame for timer logic
-    void Update()
+    // Update is called once per frame
+    protected override void Update()
     {
-        if(!playerInRange){
-            Move();
-        }
-    
-    }
-
-
-    private void Move()
-    {
-        // Calculate the new position using FixedDeltaTime.
-        Vector3 newPosition = myTransform.position + directionVector * speed * Time.fixedDeltaTime;
-        // Optional debug log to see where we're trying to move.
-        // Debug.Log("Attempting move to: " + newPosition);
-
-        // Check if the new position is within the allowed bounds.
-        if (bounds.bounds.Contains(newPosition))
+        base.Update();
+        if(isMoving)
         {
-            myRigidbody.MovePosition(newPosition);
+            moveTimeSeconds -= Time.deltaTime;
+            if(moveTimeSeconds<= 0)
+            {
+                moveTimeSeconds = Random.Range(minMoveTime, maxMoveTime);
+                isMoving = false;
+            }
+            if (!playerInRange)
+            {
+                Move();
+            }
         }
         else
         {
-            // If out of bounds, change direction.
+            waitTimeSeconds -= Time.deltaTime;
+            if(waitTimeSeconds <= 0)
+            {
+                ChooseDifferentDirection();
+                isMoving = true;
+                waitTimeSeconds = Random.Range(minWaitTime, maxWaitTime);
+            }
+        }
+    }
+
+    private void ChooseDifferentDirection()
+    {
+        Vector3 temp = directionVector;
+        ChangeDirection();
+        int loops = 0;
+        while (temp == directionVector && loops < 100)
+        {
+            loops++;
+            ChangeDirection();
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 temp = myTransform.position + directionVector * speed * Time.deltaTime;
+        if (bounds.bounds.Contains(temp))
+        {
+            myRigidbody.MovePosition(temp);
+        }
+        else
+        {
             ChangeDirection();
         }
     }
@@ -54,18 +86,22 @@ public class NPC : MonoBehaviour
     void ChangeDirection()
     {
         int direction = Random.Range(0, 4);
-        switch (direction)
+        switch(direction)
         {
             case 0:
+                // Walking to the right
                 directionVector = Vector3.right;
                 break;
             case 1:
+                // Walking up
                 directionVector = Vector3.up;
                 break;
             case 2:
+                // Walking Left
                 directionVector = Vector3.left;
                 break;
             case 3:
+                // Walking down
                 directionVector = Vector3.down;
                 break;
             default:
@@ -76,21 +112,13 @@ public class NPC : MonoBehaviour
 
     void UpdateAnimation()
     {
-        if (anim != null)
-        {
-            anim.SetFloat("MoveX", directionVector.x);
-            anim.SetFloat("MoveY", directionVector.y);
-        }
+        anim.SetFloat("MoveX", directionVector.x);
+        anim.SetFloat("MoveY", directionVector.y);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Vector3 temp = directionVector;
-        ChangeDirection();
-        int loops = 0;
-        while(temp == directionVector && loops < 100){
-            loops++;
-            ChangeDirection();
-        }
+        ChooseDifferentDirection();
     }
-} 
+
+}
